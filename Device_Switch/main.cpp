92eea10acf0e3175538c6185a51f5dadfa5095d9
ujari <144187814+ujari@users.noreply.github.com>
2024-11-25 09:03:23 +0900

    완

--- Device_Switch/main.cpp
+++ Device_Switch/main.cpp
@@ -7,20 +7,20 @@
 void Device_op(const std::string& deviceInstanceId) {
 
     DEVINST devInst;
-    ULONG s,p;
+    ULONG s, p;
     CONFIGRET cr = CM_Locate_DevNodeA(&devInst, const_cast<DEVINSTID_A>(deviceInstanceId.c_str()), CM_LOCATE_DEVNODE_NORMAL);
 
     if (cr != CR_SUCCESS) {
         std::cerr << "장치를 찾을 수 없습니다. 오류 코드: " << cr << std::endl;
     }
-    
-    //장치 상태 불러오기
-    CM_Get_DevNode_Status(&s,&p,devInst,0);
 
-    
+    //장치 상태 불러오기
+    CM_Get_DevNode_Status(&s, &p, devInst, 0);
+
+
     if (s & DN_DRIVER_LOADED)//장치가 활성화 되있는 경우
     {
-        CM_Disable_DevNode(devInst, 0);//비활성ㅌ화
+        CM_Disable_DevNode(devInst, 0);//비활성화
     }
     else CM_Enable_DevNode(devInst, 0);
 }
